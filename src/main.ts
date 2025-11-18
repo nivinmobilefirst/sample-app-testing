4ba6f5cbddce32d59b8ed72783a9bd072f0f4970
nivinmobilefirst <nivint@mobilefirstapplications.com>
2025-11-18 16:25:58 +0530

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverless from '@vendia/serverless-express';
 
-let cachedServer;
+let cachedApp;
 
-async function bootstrapServer() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.init();
-
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverless({ app: expressApp });
+  return app.getHttpAdapter().getInstance();
 }
 
-// Vercel requires a *default export* that is a function
 export default async function handler(req, res) {
-  if (!cachedServer) {
-    cachedServer = await bootstrapServer();
+  if (!cachedApp) {
+    cachedApp = await bootstrap();
   }
-  return cachedServer(req, res);
+  return cachedApp(req, res);
 }
